f1462093b225724c02ca7c72208b9846eb4e3a8e
alexcogojocaru <alex.cojocaru73@gmail.com>
2022-03-10 00:46:23 +0200

    basic configs for the agent

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 generate-proto:
 	@protoc -I=btrace-idl/proto --go_out=proto-gen .\btrace-idl\proto\agent.proto
 	@protoc -I=btrace-idl/proto --go-grpc_out=proto-gen .\btrace-idl\proto\agent.proto
+
+build:
+	@go install
+
+run:
+	@btrace-agent.exe
--- agent.go
+++ agent.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
@@ -9,17 +10,21 @@ import (
 	"google.golang.org/grpc"
 )
 
+const host = "localhost"
+const port = 4576
+
 func main() {
 	log.SetFlags(log.Lmicroseconds | log.Ldate)
 
-	lis, err := net.Listen("tcp", ":4576")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatal("Cannot create a net listener on port 4576")
+		log.Fatalf("Cannot create a net listener on port %d", port)
 	}
 
 	grpcServer := grpc.NewServer()
 	agentService := service.NewAgentService()
 
+	log.Printf("Starting Agent on %s:%d", host, port)
 	// Register the agent service to the gRPC server and serve the listener
 	agent_pb.RegisterAgentServiceServer(grpcServer, agentService)
 	if err := grpcServer.Serve(lis); err != nil {
