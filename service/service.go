c7252a3b4a75ffe31ea199b6eed0106cf53c8f88
alexcogojocaru <alex.cojocaru73@gmail.com>
2022-03-21 22:53:07 +0200

    updated agent

--- service/service.go
+++ service/service.go
@@ -5,20 +5,55 @@ import (
 	"log"
 
 	agent_pb "github.com/alexcogojocaru/btrace-agent/proto-gen/btrace_agent"
-	"google.golang.org/grpc/metadata"
+	collector_pb "github.com/alexcogojocaru/btrace-agent/proto-gen/btrace_collector"
+	"google.golang.org/grpc"
 )
 
 type AgentServiceImpl struct {
 	agent_pb.UnimplementedAgentServiceServer
+	CollectorClient collector_pb.CollectorServiceClient
+}
+
+func NormalizeSpan(span *agent_pb.Span) collector_pb.SpanC {
+	return collector_pb.SpanC{
+		Name: span.Name,
+		CurrentContext: &collector_pb.ContextC{
+			TraceID: span.CurrentContext.TraceID,
+			SpanID:  span.CurrentContext.SpanID,
+		},
+		ParentContext: &collector_pb.ContextC{
+			TraceID: span.ParentContext.TraceID,
+			SpanID:  span.ParentContext.SpanID,
+		},
+		Timestamp: &collector_pb.TimestampC{
+			Started:  span.Timestamp.Started,
+			Ended:    span.Timestamp.Ended,
+			Duration: span.Timestamp.Duration,
+		},
+	}
 }
 
 func NewAgentService() *AgentServiceImpl {
-	return &AgentServiceImpl{}
+	conn, err := grpc.Dial("localhost:4578", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("Cannot dial localhost:4578")
+	}
+
+	client := collector_pb.NewCollectorServiceClient(conn)
+
+	return &AgentServiceImpl{
+		CollectorClient: client,
+	}
 }
 
 func (agentServiceImpl *AgentServiceImpl) StreamSpan(ctx context.Context, span *agent_pb.Span) (*agent_pb.Response, error) {
-	meta, _ := metadata.FromIncomingContext(ctx)
-	log.Print(meta)
+	// meta, _ := metadata.FromIncomingContext(ctx)
+	// log.Print(meta)
+	log.Print(span)
+
+	collectorSpan := NormalizeSpan(span)
+	agentServiceImpl.CollectorClient.StreamSpan(ctx, &collectorSpan)
+
 	return &agent_pb.Response{}, nil
 }
 
