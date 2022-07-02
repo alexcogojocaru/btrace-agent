bae4324ba18a6ada8fdd25dadf94f859b3cba21a
alexcogojocaru <alex.cojocaru73@gmail.com>
2022-07-02 23:00:46 +0300

    config parser for connections

--- agent.go
+++ agent.go
@@ -4,41 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"
 
+	"github.com/alexcogojocaru/btrace-agent/config"
 	agent "github.com/alexcogojocaru/btrace-agent/proto-gen/btrace_proxy"
 	service "github.com/alexcogojocaru/btrace-agent/service"
 	"google.golang.org/grpc"
 )
 
-const host = "localhost"
-const port = 4576
-
 func main() {
 	log.SetFlags(log.Lmicroseconds | log.Ldate)
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	conf, err := config.ParseConfig("config/config.yml")
 	if err != nil {
-		log.Fatalf("Cannot create a net listener on port %d", port)
+		log.Fatal("Error while parsing the config file")
+	}
+
+	uri := fmt.Sprintf("%s:%d", conf.Deploy.Hostname, conf.Deploy.Port)
+	collectorUri := fmt.Sprintf("%s:%d", conf.Connections.Collector.Hostname, conf.Connections.Collector.Port)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", conf.Deploy.Port))
+	if err != nil {
+		log.Fatalf("Cannot create a net listener on port %d", conf.Deploy.Port)
 	}
 
 	grpcServer := grpc.NewServer()
-	agentService := service.NewAgentService()
+	agentService := service.NewAgentService(collectorUri)
 
-	var waitGroup sync.WaitGroup
-
-	waitGroup.Add(1)
-	go func() {
-		defer waitGroup.Done()
-
-		log.Printf("Starting Agent on %s:%d", host, port)
-
-		// Register the agent service to the gRPC server and serve the listener
-		agent.RegisterAgentServer(grpcServer, agentService)
-		if err := grpcServer.Serve(lis); err != nil {
-			log.Fatal("Failed to serve the gRPC server")
-		}
-	}()
-
-	waitGroup.Wait()
+	log.Printf("Agent started on %s", uri)
+	agent.RegisterAgentServer(grpcServer, agentService)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal("Failed to serve the gRPC server")
+	}
 }
--- service/service.go
+++ service/service.go
@@ -3,31 +3,69 @@ package service
 import (
 	"context"
 	"log"
+	"time"
 
 	agent "github.com/alexcogojocaru/btrace-agent/proto-gen/btrace_proxy"
 	"google.golang.org/grpc"
 )
 
+const POLLING_INTERVAL = 1
+const BUFFER_SIZE = 50
+
 type AgentServiceImpl struct {
-	CollectorClient agent.AgentClient
 	agent.UnimplementedAgentServer
+	CollectorClient agent.AgentClient
+	Buffer          chan *agent.Span
 }
 
-func NewAgentService() *AgentServiceImpl {
-	agentHost := "localhost:4578"
-	conn, err := grpc.Dial(agentHost, grpc.WithInsecure())
+func NewAgentService(collectorUri string) *AgentServiceImpl {
+	conn, err := grpc.Dial(collectorUri, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("Cannot dial %s", agentHost)
+		log.Fatalf("Cannot dial %s", collectorUri)
 	}
 
-	return &AgentServiceImpl{
+	service := &AgentServiceImpl{
 		CollectorClient: agent.NewAgentClient(conn),
+		Buffer:          make(chan *agent.Span, BUFFER_SIZE),
+	}
+
+	go func() {
+		service.Poll(context.Background())
+	}()
+
+	return service
+}
+
+func (ag *AgentServiceImpl) Poll(ctx context.Context) {
+	for {
+		bufferSize := len(ag.Buffer)
+		if bufferSize > 0 || bufferSize == BUFFER_SIZE {
+			log.Printf("batch size %d", bufferSize)
+			stream, err := ag.CollectorClient.Stream(ctx)
+			if err != nil {
+				log.Fatalf("Stream error %v", err)
+			}
+
+			for idx := 0; idx < bufferSize; idx++ {
+				span := <-ag.Buffer
+				log.Print(span)
+				if err := stream.Send(span); err != nil {
+					log.Fatalf("Send error %v", err)
+				}
+			}
+
+			_, err = stream.CloseAndRecv()
+			if err != nil {
+				log.Fatalf("CloseAndRecv err %v", err)
+			}
+		}
+
+		time.Sleep(POLLING_INTERVAL)
 	}
 }
 
 func (agentServiceImpl *AgentServiceImpl) Send(ctx context.Context, span *agent.Span) (*agent.Response, error) {
-	log.Print(span)
-	agentServiceImpl.CollectorClient.Send(ctx, span)
-
+	// agentServiceImpl.CollectorClient.Send(ctx, span)
+	agentServiceImpl.Buffer <- span
 	return &agent.Response{}, nil
 }
